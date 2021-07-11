ee93002da375b55b675551f54f6269be27f74bdd
Damup1988 <damup1988@gmail.com>
2021-07-11 16:22:11 +0300

    All CRUDs have been implemented with MongoDB rep

--- Catalog.Repository/ItemRepositoryMongoDb.cs
+++ Catalog.Repository/ItemRepositoryMongoDb.cs
@@ -1,44 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using Catalog.Domain;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Catalog.Repository
 {
     public class ItemRepositoryMongoDb : IItemRepository
     {
-        private const string databaseName = "catalog";
-        private const string collectionName = "items";
-        private readonly IMongoCollection<Item> itemsCollection;
+        private const string DatabaseName = "catalog";
+        private const string CollectionName = "items";
+        private readonly IMongoCollection<Item> _itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> _filterBuilder = Builders<Item>.Filter;
         
         public ItemRepositoryMongoDb(IMongoClient mongoClient)
         {
-            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
-            itemsCollection = database.GetCollection<Item>(collectionName);
+            IMongoDatabase database = mongoClient.GetDatabase(DatabaseName);
+            _itemsCollection = database.GetCollection<Item>(CollectionName);
         }
         public IEnumerable<Item> GetItems()
         {
-            throw new NotImplementedException();
+            return _itemsCollection.Find(new BsonDocument()).ToList();
         }
 
         public Item GetItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = _filterBuilder.Eq(item => item.Id, id);
+            return _itemsCollection.Find(filter).SingleOrDefault();
         }
 
         public void CreateItem(Item item)
         {
-            itemsCollection.InsertOne(item);
+            _itemsCollection.InsertOne(item);
         }
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            var filter = _filterBuilder.Eq(existringItem => existringItem.Id, item.Id);
+            _itemsCollection.ReplaceOne(filter, item);
         }
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = _filterBuilder.Eq(item => item.Id, id);
+            _itemsCollection.DeleteOne(filter);
         }
     }
 }
--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Catalog.Repository;
+using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,25 +13,39 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 namespace Catalog
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //need to convert Guid and DateTimeOffset to a string (inside a DB)
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            
+            services.AddSingleton<IMongoClient>(serviceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
             services.AddControllers();
-            services.AddSingleton<IItemRepository, ItemRepository>();
+            //services.AddSingleton<IItemRepository, ItemRepository>();
             services.AddSingleton<IItemRepository, ItemRepositoryMongoDb>();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Catalog", Version = "v1"}); });
+            services.AddSwaggerGen(c => 
+                { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Catalog", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +55,8 @@ namespace Catalog
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
+                app.UseSwaggerUI(c => 
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
             app.UseHttpsRedirection();
